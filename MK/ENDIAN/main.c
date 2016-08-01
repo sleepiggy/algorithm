dca7dc9f0e66f70983f47265ea3d6f3c6ac0ad91
K <K@Kui-MacBook-Pro.local>
2016-08-01 21:37:00 +0900

    [ENDIAN][MK] Complete ENDIAN code.
    Code was accepted on algospot.
    - 769KB
    - 7ms

--- MK/ENDIAN/main.c
+++ MK/ENDIAN/main.c
@@ -13,12 +13,26 @@ int main(int argc, const char * argv[])
 {
     FILE *fp;
     int test_cnt;
+    int i, j;
+    unsigned long in, out;
     
-    fp = fopen("/Users/K/work/algorithm/MK/ENDIAN/input.txt","r");
+    //fp = fopen("/Users/K/work/algorithm/MK/ENDIAN/input.txt","r");
+    fp = stdin;
     fscanf(fp, "%d ", &test_cnt);
 
-    printf("%d", test_cnt);
-    
+    for ( i = 0 ; i < test_cnt ; i++)
+    {
+        out = 0;
+        fscanf(fp, "%ld ", &in);
+        for(j = 0 ; j < 4 ; j ++)
+        {
+            out += (in & 0xff);
+            if(j == 3) break;
+            in = (in >> 8);
+            out = (out << 8);
+        }
+        printf("%ld\n",out);
+    }
     fclose(fp);
     
     return 0;
