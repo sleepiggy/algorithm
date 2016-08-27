33bcfab66bbfe44f7dbe27b6eaaaae6070d7fc9b
김보근 <fwhalee@gmail.com>
2016-08-27 12:29:10 +0900

    사이즈값 조절 답안제출시 시간초과

--- fwhalee/CodeTest/src/com/fwhalee/code/Quantization.java
+++ fwhalee/CodeTest/src/com/fwhalee/code/Quantization.java
@@ -1,11 +1,14 @@
 package com.fwhalee.code;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Quantization {
     
     
-    private static ArrayList<Integer> sequence = new ArrayList<Integer>();
+	// 수열 데이터
+    private static ArrayList<Integer> sequence;
     
     
     public static void main(String[] args) {
@@ -13,44 +16,29 @@ public class Quantization {
         
         try {
             
-//            Scanner reader = new Scanner(System.in);
+            Scanner reader = new Scanner(System.in);
             
-//            int firstLine = reader.nextInt();
-            for (int i = 0; i < 1; i++) {
+            int firstLine = reader.nextInt();
+            for (int i = 0; i < firstLine; i++) {
                 
-                // 수열 개수, 최대 양자화 수 받기
-                // long line = reader.nextLong();
-                int quantizeNum = 3;
+            	sequence = new ArrayList<Integer>();
+            	
+                // 수열 개수
+            	int sequenceNum = reader.nextInt();
+            	// 최대 양자화 수
+                int quantizeNum = reader.nextInt();
                 
-                // 수열 받기
-                /*sequence.add(1);
-                sequence.add(2);
-                sequence.add(3);
-                sequence.add(4);*/
+                // 수열 입력
+                for (int j = 0; j < sequenceNum; j++) {
+                	sequence.add(reader.nextInt());
+                }
                 
-                /*sequence.add(1);
-                sequence.add(1);
-                sequence.add(2);
-                sequence.add(2);
-                sequence.add(2);
-                sequence.add(2);
-                sequence.add(3);
-                sequence.add(3);
-                sequence.add(3);
-                sequence.add(3);
-                */
+                // 수열 오름차순 정렬
+                Collections.sort(sequence);
                 
-                sequence.add(1);
-                sequence.add(4);
-                sequence.add(6);
-                sequence.add(744);
-                sequence.add(755);
-                sequence.add(777);
-                sequence.add(890);
-                sequence.add(897);
-                sequence.add(902);
                 int result = getTotalMinError(quantizeNum, 0);
-                System.out.println("total Min Error: " + result);
+//                System.out.println(result);
+                System.out.println("Total Min Error:" + result);
                 
             }
             
@@ -64,24 +52,17 @@ public class Quantization {
     public static int getTotalMinError(int quantizeNum, int offset) {
         
         System.out.println("quantizeNum: " + quantizeNum + " / offset: " + offset);
-        if (quantizeNum == 0) return Integer.MAX_VALUE;
-        if (offset == sequence.size()) {
-        	System.out.println("exit");
-        	return 0;
-        }
+        
         if (quantizeNum == 1) {
-            // S가 1일 때 최소 오차 구하기. return
         	return getMinError(offset, sequence.size() - offset);
         }
+        
         int minError = Integer.MAX_VALUE;
         
         // 수열 분할에 대한 recursive
         for (int i = 0; i < (sequence.size() - offset - quantizeNum + 1); i++) {
-            
             int tempMinError = getMinError(offset, i + 1) + getTotalMinError(quantizeNum - 1, offset + i + 1);
-            if (minError >= tempMinError) {
-            	minError = tempMinError;
-            }
+            minError = Math.min(minError, tempMinError);
         }
         
         return minError;
@@ -89,24 +70,25 @@ public class Quantization {
     
     
     public static int getMinError(int offset, int size) {
+    	
+    	System.out.println("offset:" + offset + " / size: " + size);
         
-        // 평균 구하기
+        // offset ~ end 데이터 평균 구하기
         int sum = 0;
         for (int i = offset; i < (offset + size); i++) {
             sum += sequence.get(i);
         }
         
-        // 반올림
-        int avg = Math.round(sum / size);
+        // 평균은 가까운 정수로 반올림
+        int avg = Math.round((float) sum / (float) size);
+        
         int error = 0;
-        System.out.println("offset:" + offset + " / size: " + size);
         for (int i = offset; i < (offset + size); i++) {
-        	System.out.print(sequence.get(i) + "|");
+        	System.out.print(sequence.get(i) + " | ");
             error += (int) Math.pow((sequence.get(i) - avg), 2);
         }
-        System.out.print("error: " + error);
-        System.out.println("");
-        System.out.println("");
+        System.out.println("\nerror: " + error);
+        System.out.println("\n");
         return error;
     }
     
