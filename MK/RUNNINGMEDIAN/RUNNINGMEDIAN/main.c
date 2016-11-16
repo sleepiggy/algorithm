b703c9d6f4c14a369de4ba11d7d3a4badc3a3c1f
Minkyoung Kim <sleepigmk@gmail.com>
2016-11-16 20:44:53 +0900

    [RUNNINGMEDIAN][MK] Not complete code.

---
+++ MK/RUNNINGMEDIAN/RUNNINGMEDIAN/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RUNNINGMEDIAN
+//
+//  Created by K on 2016. 11. 8..
+//  Copyright © 2016년 K. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
